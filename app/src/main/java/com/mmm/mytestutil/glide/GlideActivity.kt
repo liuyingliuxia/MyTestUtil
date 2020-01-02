e378777cf783fe2822c378b32eb69118abbee7bc
夏 小 <1652976019@qq.com>
2020-01-03 00:52:17 +0800

    添加了左侧抽屉布局， 网络图片加载 ，准备做： 双表联动， 上抽屉,rv嵌套rv --goodNight

--- app/src/main/java/com/mmm/mytestutil/glide/GlideActivity.kt
+++ app/src/main/java/com/mmm/mytestutil/glide/GlideActivity.kt
@@ -13,8 +13,13 @@ class GlideActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_glide)
-        Glide.with(this).load("http://f.hiphotos.baidu.com/zhidao/pic/item/8ad4b31c8701a18bcab7e37a9d2f07082838fea3.jpg").into(ivImage1)
-        Glide.with(this).load(R.drawable.fruit_10).into(ivImage2)
+        Glide.with(this).load("http://f.hiphotos.baidu.com/zhidao/pic/item/8ad4b31c8701a18bcab7e37a9d2f07082838fea3.jpg")
+            .placeholder(R.drawable.place_holder_product_category_image).error(R.drawable.icon_coupon_all_not_usable).into(ivImage1)
+        Glide.with(this).load("https://img3.doubanio.com/view/photo/m/public/p2577340942.webp")
+            .placeholder(R.drawable.place_holder_product_category_image).error(R.drawable.icon_coupon_all_not_usable).into(ivImage2)
+
+        Glide.with(this).load(R.drawable.fruit_10).into(ivImage3)
+        Glide.with(this).load(R.drawable.default_avatar_3).into(ivImage4)
     }
 
 /*    fun getView(position: Int, recycled: View?, container: ViewGroup?): View {
