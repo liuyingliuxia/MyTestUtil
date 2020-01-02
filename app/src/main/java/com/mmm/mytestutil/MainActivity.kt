67534333845c75b01514ba535fbc496d201cf5b4
liuyingliuxia <liuyingliuxia@163.com>
2020-01-02 16:48:09 +0800

    添加 Glide

--- app/src/main/java/com/mmm/mytestutil/MainActivity.kt
+++ app/src/main/java/com/mmm/mytestutil/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mmm.mytestutil
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.mmm.mytestutil.glide.GlideActivity
 import com.mmm.mytestutil.vp2.Vp2TestActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,11 +13,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         tvVp2.setOnClickListener(this)
+        tvGlide.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
         when(v){
             tvVp2 -> myUtil.jumpView(this,Vp2TestActivity())
+            tvGlide -> myUtil.jumpView(this,GlideActivity())
         }
     }
 }
---
+++ app/src/main/java/com/mmm/mytestutil/glide/GlideActivity.kt
@@ -0,0 +1,37 @@
+package com.mmm.mytestutil.glide
+
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.mmm.mytestutil.R
+import kotlinx.android.synthetic.main.activity_glide.*
+
+class GlideActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_glide)
+       // Glide.with(this).load("http://f.hiphotos.baidu.com/zhidao/pic/item/8ad4b31c8701a18bcab7e37a9d2f07082838fea3.jpg").into(ivImage1) 获取 url失败
+        Glide.with(this).load(R.drawable.fruit_10).into(ivImage2)
+    }
+
+/*    fun getView(position: Int, recycled: View?, container: ViewGroup?): View {
+        val myImageView: ImageView
+        myImageView = if (recycled == null) {
+            inflater.inflate(R.layout.my_image_view, container, false) as ImageView
+        } else {
+            recycled as ImageView
+        }
+        val url: String = myUrls.get(position)
+        Glide
+            .with(myFragment)
+            .load(url)
+            .centerCrop()
+            .placeholder(R.drawable.loading_spinner)
+            .crossFade()
+            .into(myImageView)
+        return myImageView
+    }*/
+}
